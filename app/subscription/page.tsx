2099d9076f21b7ebf1b7673cd7451da50a7f80af
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2025-11-22 00:15:33 +0000

    feat: ensure transactions and subscriptions pages are protected

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,7 +1,34 @@
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import AddTransactionButton from "../_components/add-transaction-button";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { transactionColumns } from "../transactions/_columns";
 
-const SubscriptionPage = () => {
-  return <Navbar />;
+const TransactionsPage = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: {
+      userId,
+    },
+  });
+  return (
+    <>
+      <Navbar />
+      <div className="space-y-6 p-6">
+        {/* TÍTULO E BOTÃO */}
+        <div className="flex w-full items-center justify-between">
+          <h1 className="text-2xl font-bold">Transações</h1>
+          <AddTransactionButton />
+        </div>
+        <DataTable columns={transactionColumns} data={transactions} />
+      </div>
+    </>
+  );
 };
 
-export default SubscriptionPage;
+export default TransactionsPage;
